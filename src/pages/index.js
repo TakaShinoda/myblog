7a5a072e18a496a9c70e0b426b253db88853a99f
Takahiro <tkhr_621@docomo.ne.jp>
2020-06-22 23:07:18 +0900

    Fix: 画像を表示

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="container">
           <div className="site">
             <a href="base-index.html">
-              <img src="images/logo.svg" alt="ESSENTIALS" />
+              <img src="/images/logo.svg" alt="ESSENTIALS" />
             </a>
           </div>
           <nav className="nav">
@@ -24,7 +24,7 @@ export default function Home() {
       </header>
       <section className="hero">
         <figure>
-          <img src="images/hero.jpg" alt="" />
+          <img src="/images/hero.jpg" alt="" />
         </figure>
         <div className="catch">
           <h1>
@@ -34,7 +34,7 @@ export default function Home() {
           <p>食物を愛するよりも誠実な愛はない ― バーナード・ショー</p>
         </div>
         <div className="wave">
-          <img src="images/wave.svg" alt="" />
+          <img src="/images/wave.svg" alt="" />
         </div>
       </section>
       <section className="food">
@@ -45,7 +45,7 @@ export default function Home() {
           <div className="details">
             <div className="detail">
               <figure>
-                <img src="images/fruit.jpg" alt="" />
+                <img src="/images/fruit.jpg" alt="" />
               </figure>
               <h3>フルーツ</h3>
               <p>FRUIT</p>
@@ -57,7 +57,7 @@ export default function Home() {
             </div>
             <div className="detail">
               <figure>
-                <img src="images/grain.jpg" alt="" />
+                <img src="/images/grain.jpg" alt="" />
               </figure>
               <h3>穀物</h3>
               <p>GRAIN</p>
@@ -69,7 +69,7 @@ export default function Home() {
             </div>
             <div className="detail">
               <figure>
-                <img src="images/beverage.jpg" alt="" />
+                <img src="/images/beverage.jpg" alt="" />
               </figure>
               <h3>飲み物</h3>
               <p>BEVERAGE</p>
@@ -85,14 +85,14 @@ export default function Home() {
       <section className="photo">
         <h2 className="sr-only">Photo</h2>
         <figure>
-          <img src="images/berry.jpg" alt="赤く熟したベリー" />
+          <img src="/images/berry.jpg" alt="赤く熟したベリー" />
         </figure>
       </section>
       <footer className="footer">
         <div className="container">
           <div className="site">
             <a href="base-index.html">
-              <img src="images/logo-w.svg" alt="ESSENTIALS" />
+              <img src="/images/logo-w.svg" alt="ESSENTIALS" />
               <p>おいしい食材と食事を探求するサイト</p>
             </a>
           </div>
