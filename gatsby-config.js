f6196d0e8c83f3ba7582f05013d92a7d26e25056
Takahiro <tkhr_621@docomo.ne.jp>
2020-06-23 00:59:28 +0900

    Add: 設定を追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,20 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+ // 作成するサイトのメタデータや、プラグインの設定を始め
+ // サイトの構成を設定すファイル
+ 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
