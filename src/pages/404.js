6392165e40b698d2a0171c41a929a7b862080274
Takahiro <tkhr_621@docomo.ne.jp>
2020-06-28 16:12:13 +0900

    Add: 404ページを作成

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Layout } from "../components/Layout"
+
+export default () => (
+  <Layout>
+    <h1 style={{ padding: "30vh 0", textAlign: "center" }}>お探しのページは見つかりませんでした</h1>
+  </Layout>
+)
