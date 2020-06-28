bccadee4c5e10e0aa955218ae031bfc0345e783d
Takahiro <tkhr_621@docomo.ne.jp>
2020-06-28 15:50:00 +0900

    Add: FontAwesomeのcss設定を追加

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,11 @@ import React from "react"
 import { Header } from "../components/header"
 import { Footer } from "../components/Footer"
 import "./layout.css"
+// FontAwesomeのcssを先読みする設定
+import "@fortawesome/fontawesome-svg-core/style.css"
+// FontAwesomeのコンポーネント内で、cssを適用しないようにする設定
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
 
 export const Layout = ({ children }) => (
   <div>
