f249486b76f143efb94230df4ea83ec5b735274f
Takahiro <tkhr_621@docomo.ne.jp>
2020-06-28 17:57:58 +0900

    Add: react-helmetを追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
- // 作成するサイトのメタデータや、プラグインの設定を始め
- // サイトの構成を設定すファイル
- 
+// 作成するサイトのメタデータや、プラグインの設定を始め
+// サイトの構成を設定すファイル
+
 module.exports = {
   /* Your site config here */
   plugins: [
@@ -19,5 +19,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-react-helmet`,
   ],
 }
