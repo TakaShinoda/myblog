57f51e8d6d08e481c06705f71e9b5191cadb65c0
Takahiro <tkhr_621@docomo.ne.jp>
2020-06-28 20:53:25 +0900

    Fix: スタイルの修正

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import { SEO } from "../components/SEO"
 export default ({ location }) => (
   <Layout>
     <SEO pagetitle="ページが見つかりません" pagepath={location.pathname}/>
-    <h1 style={{ padding: "30vh 0", textAlign: "center" }}>
+    <h1 style={{ padding: "26vh 0", textAlign: "center" }}>
       お探しのページは見つかりませんでした
     </h1>
   </Layout>
