a8344821e96d5812e2f2e935b29bef6b180f180c
Takahiro <tkhr_621@docomo.ne.jp>
2020-07-02 22:31:36 +0900

    feat: PWA対応の設定を追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,5 +28,18 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ESSENTIALS エッセンシャルズ`,
+        short_name: `ESSENTIALS`,
+        start_url: `/`,
+        backgroud_color: `#ffffff`,
+        theme_color: `#477294`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
